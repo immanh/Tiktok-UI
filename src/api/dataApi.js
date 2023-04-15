94f82f36e44393e60a922a3ba56be93903a25697
Duc Manh <ducmanhdut248@gmail.com>
2023-04-15 23:09:45 +0700

    add customAvatar and customInfo prop

---
+++ src/api/dataApi.js
@@ -0,0 +1,24 @@
+const suggestedAcc = [
+    {
+        avatar: 'https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/9edf82267911a307f4c0775f99a54d47~c5_100x100.jpeg?x-expires=1681653600&x-signature=z0ryT9PrsDVpxgLhr5WUDh2SOY4%3D',
+        full_name: 'podcastcuaanday',
+        nickname: 'Ân kể chuyện',
+        tick: true,
+        live: true,
+    },
+    {
+        avatar: 'https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-aiso/65d3c6b1d1e205c75536ccf1f26d552d~c5_100x100.jpeg?x-expires=1681740000&x-signature=0dFdPf22qIoJrJAzVTjE0CwiMn0%3D',
+        full_name: 'theanh28entertainment',
+        nickname: 'Theanh28 Entertainment',
+        tick: true,
+        live: false,
+    },
+    {
+        avatar: 'https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/31cbdd30c603fd8103646ae97665e319~c5_100x100.jpeg?x-expires=1681743600&x-signature=LeXOusG%2BDsdEDKsppgLFK8GcO%2Fw%3D',
+        full_name: 'nguyenhuynhnhubossx3',
+        nickname: 'Nguyễn Huỳnh Như',
+        tick: true,
+        live: false,
+    },
+];
+export { suggestedAcc };
