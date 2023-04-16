105d05fb91ff96ff369da47d5342976b966c489e
Duc Manh <ducmanhdut248@gmail.com>
2023-04-16 16:27:20 +0700

    fix overflow text bug account

--- src/api/dataApi.js
+++ src/api/dataApi.js
@@ -8,7 +8,7 @@ const suggestedAcc = [
     },
     {
         avatar: 'https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-aiso/65d3c6b1d1e205c75536ccf1f26d552d~c5_100x100.jpeg?x-expires=1681740000&x-signature=0dFdPf22qIoJrJAzVTjE0CwiMn0%3D',
-        full_name: 'theanh28entertainment',
+        full_name: 'theanh28entertainmentssssssssssssssssssssssssssssssssssssss',
         nickname: 'Theanh28 Entertainment',
         tick: true,
         live: false,
